f3be4e66952c7cff6f6dfcbf44d68cd20134defd
Kingwell <jinhua.leng@gmail.com>
2016-11-17 11:36:35 +0800

    add

--- README.md
+++ README.md
@@ -6,6 +6,20 @@
 
 ### Require
 	var utils = require('npm-utils-kingwell');
+
+
+1. objectSort
+2. deepCopy
+3. slice
+4. inArray
+5. toDenseArray
+6. delDupArray||delArray @description Delete Duplicates Array
+7. strimHtml
+8. getTimesTamp
+9. fixNumber
+10. getDateString
+11. log||log.help()||success()||error()||info()||warn();
+
 ### Object sort
 
     /**
@@ -58,6 +72,10 @@
 
 ![](images/color-demo1.png)
 
+### utils.log(msg,colorStyle)// colorStyle=> red,green,brown,blue....
+#### utils.log.help() //output 
+![](images/style.png)
+
 ### utils.success(msg);
 > output color //green
 
@@ -80,18 +98,36 @@
 	console.log(utils.slice(str,3,'-'));//output 'abcd-'
 	console.log(utils.slice(str,3,'+'));//output 'abcd+'
 
-### Get date string
-	getDateString(Date);
-	console.log(utils.getDateString(new Date)); //output 2016-08-10 11:32:00
+
+### fixNumber
+	
+	utils.fixNumber(1,2);//output '01'
+	utils.fixNumber(1,3);//output '001'
+	utils.fixNumber(1,4);//output '0001'
+	utils.fixNumber(1,5);//output '00001'
+
+	utils.fixNumber.size = 3;
+	utils.fixNumber(1);//output '001'
+	utils.fixNumber(12);//output '012'
+	utils.fixNumber(120);//output '120'
+
+### getDateString
+	getDateString(Date date,Boolean showTime);
+	console.log(utils.getDateString(new Date)); //output 2016-08-10
+	console.log(utils.getDateString(new Date),true); //output 2016-08-10 11:32:00
+
 ### toDenseArray
 	var arr = [1,2,3,'',4];
 	utils.toDenseArray(arr) //output [1,2,3,4]
+
 ### delArray
 	var arr = [1,2,2,3,3,3,3,4];
 	utils.delArray(arr); //output [1,2,3,4]
+
 ### inArray
 	var arr = ['a','b','c']
 	utils.inArray(b,arr); //output 1
+
 ### strimHtml
 	var str = '<div>html</div>';
 	utils.strimHtml(str);//output html;
--- index.js
+++ index.js
@@ -1,202 +1,326 @@
- var utils = {
- 	slice: function(str, length, symbol) {
- 		var result = '',
- 			sym = symbol || '...';
+(function(root, undefined) {
 
- 		if (!str || typeof str !== 'string' || !length) {
- 			result = str;
- 		} else {
- 			if (str.length < length) {
- 				sym = '';
- 			}
- 			result = str.slice(0, length) + sym;
- 		}
 
- 		return result;
- 	},
- 	fixNumber: function(num) {
- 		if (num < 10) {
- 			return '0' + num;
- 		} else {
- 			return num;
- 		}
- 	},
- 	getDateString: function(str) {
- 		var DATE = str ? new Date(str) : new Date(),
- 			resultDate = [],
- 			resultTime = [];
- 		resultDate.push(DATE.getFullYear());
- 		resultDate.push(this.fixNumber(DATE.getMonth()));
- 		resultDate.push(this.fixNumber(DATE.getDate()));
- 		resultTime.push(this.fixNumber(DATE.getHours()));
- 		resultTime.push(this.fixNumber(DATE.getMinutes()));
- 		resultTime.push(this.fixNumber(DATE.getSeconds()));
- 		return resultDate.join('-') + ' ' + resultTime.join(':');
- 	},
- 	deepCopy: function(source) { //Copey
- 		var result = {};
- 		if (!source) {
- 			return source
- 		}
- 		if (typeof source !== 'object') {
- 			return source;
- 		}
- 		if (Object.prototype.toString.call(source).slice(8, -1) === 'Array') {
- 			result = [];
- 			for (var i = 0; i < source.length; i++) {
- 				result[i] = utils.deepCopy(source[i]);
- 			}
- 			return result;
- 		}
- 		for (var key in source) {
- 			//if (source.hasOwnProperty(key)) {
- 			result[key] = utils.deepCopy(source[key]);
- 			//}
+	/**
+	 * @description Extended Object.keys
+	 * @param  {[Object]} source [source object]
+	 * @return {[Array]}        [keys]
+	 */
+	Object.keys = Object.keys || function(source) {
+		var keys = [];
+		for (var key in source) {
+			keys.push(key);
+		}
+		return keys;
+	};
 
- 		}
- 		return result;
- 	},
- 	extend: function(tagert, source, deep) {
- 		var subObj = tagert || {};
- 		var parentObj = source || {};
- 		//parentObj = utils.deepCopy(parentObj);
- 		for (var key in parentObj) {
- 			subObj[key] = parentObj[key];
- 			sdfasd
- 		}
- 		return subObj;
- 	},
- 	getNonceStr: function(length) {
- 		var len = length || 36;
- 		return Math.random().toString(36).substr(2, len);
- 	},
- 	getTimesTamp: function() {
- 		return parseInt(new Date().getTime() / 1000) + '';
- 	},
- 	/**
- 	 * objectSort
- 	 * @param  {Object}  source object
- 	 * @param  {Number}  0 Or -1
- 	 * @return {Object}        new Object;
- 	 */
- 	objectSort: function(source, sort) {
- 		'use strict';
- 		var oldObject = source || {};
- 		var newObject = {};
- 		var _sort = sort || 0;
- 		var keys;
- 		var compareNumbers = function(a, b) {
- 			var z;
- 			if (_sort < 0) {
- 				z = a;
- 				a = b;
- 				b = z;
- 			}
- 			if (a > b) {
- 				return 1;
- 			} else if (a < b) {
- 				return -1;
- 			} else {
- 				return 0;
- 			}
- 		};
- 		keys = Object.keys(oldObject).sort(compareNumbers);
- 		keys.forEach(function(item) {
- 			newObject[item] = oldObject[item];
- 		});
- 		return newObject;
- 	},
- 	error: function(msg) {
- 		console.log(this.colorsStart['red'], msg, this.colorsEnd);
- 	},
- 	success: function(msg) {
- 		console.log(this.colorsStart['green'], msg, this.colorsEnd);
- 	},
- 	info: function(msg) {
- 		console.log(this.colorsStart['lightCyan'], msg, this.colorsEnd);
- 	},
- 	warn: function(msg) {
- 		console.log(this.colorsStart['yellow'], msg, this.colorsEnd);
- 	},
- 	colorsStart: {
- 		black: '\033[22;30m',
- 		red: '\033[22;31m',
- 		green: '\033[22;32m',
- 		brown: '\033[22;33m',
- 		blue: '\033[22;34m',
- 		magenta: '\033[22;35m',
- 		cyan: '\033[22;36m',
- 		gray: '\033[22;37m',
- 		darkGray: '\033[01;30m',
- 		lightRed: '\033[01;31m',
- 		lightGreen: '\033[01;32m',
- 		yellow: '\033[01;33m',
- 		lightBlue: '\033[01;34m',
- 		lightMagenta: '\033[01;35m',
- 		lightCyan: '\033[01;36m',
- 		white: '\033[01;37m'
- 	},
- 	colorsEnd: '\033[0m',
- 	log: function(obj, type, options) {
+	/**
+	 * @description Utils Function
+	 * @class Utils
+	 * @constructor Utils
+	 * @return {[type]} [description]
+	 * @version 1.2.0
+	 * @author Kingwell Leng <kingwell.leng@gmail.com>
+	 * @copyright kingwell 2016
+	 */
+	var Utils = function() {},
+		fn = Utils.prototype;
 
- 		var ops = options || {};
- 		var showTime = ops.showTime === false ? false : true;
- 		var line = '';
- 		var lineArr = [];
- 		for (var l = 0; l < 50; l++) {
- 			lineArr.push(line);
- 		}
- 		line = lineArr.join('');
- 		var end = '\033[0m';
- 		console.log((this.colorsStart[type] || '') + '\n' + line);
- 		if (ops.showTime) {
- 			console.log(new Date());
- 		}
- 		console.log(obj);
- 		console.log('\n' + line + end + '\n');
- 	},
- 	inArray: function(item, array) {
- 		for (var i = 0, len = array.length; i < len; i++) {
- 			if (array[i] === item) {
- 				return i;
- 			}
- 		}
- 		return -1;
- 	},
- 	//变为稠密数据
- 	toDenseArray: function(array) {
- 		if (Array.isArray(array)) {
- 			return array.filter(function(item) {
- 				return item;
- 			});
- 		} else {
- 			return array;
- 		}
- 	},
- 	//删除重复项
- 	delArray: function(array) {
- 		var n = [];
- 		var arr = array || [];
+	/**
+	 * @description Object Key Sort
+	 * @param  {[Object]} source [source object]
+	 * @param  {[Number]} sort   [0 Or -1]
+	 * @return {[Object]}        [new object]
+	 */
+	fn.objectSort = function(source, sort) {
 
- 		if (arr.length < 1) {
- 			return arr
- 		}
- 		for (var i = 0; i < arr.length; i++) {
- 			var st = false
- 			for (var j = 0; j < n.length; j++) {
- 				if (arr[i].toString() == n[j].toString()) {
- 					st = true;
- 				}
- 			}
- 			if (!st) {
- 				n.push(arr[i]);
- 			}
- 		}
- 		return n;
- 	},
- 	strimHtml: function(str) {
- 		var reg = /<(?:.|\s)*?>/ig;
- 		return str.replace(reg, '');
- 	}
- };
- module.exports = utils;
+		var sourceObject = source || {},
+			targetObject = {},
+			_sort = sort || 0,
+			keys,
+			compareNumbers = function(a, b) {
+				var z;
+				if (_sort < 0) {
+					z = a;
+					a = b;
+					b = z;
+				}
+				if (a > b) {
+					return 1;
+				} else if (a < b) {
+					return -1;
+				} else {
+					return 0;
+				}
+			};
+		keys = Object.keys(sourceObject).sort(compareNumbers);
+		keys.forEach(function(item) {
+			targetObject[item] = sourceObject[item];
+		});
+		return targetObject;
+	};
+
+	/**
+	 * @description Deep Copy
+	 * @param  {[Object]} source [source object]
+	 * @return {[Object]}        [new object]
+	 */
+	fn.deepCopy = function copy(source) {
+		var result = {};
+		if (!source) {
+			return source
+		}
+		if (typeof source !== 'object') {
+			return source;
+		}
+		if (Object.prototype.toString.call(source).slice(8, -1) === 'Array') {
+			result = [];
+			for (var i = 0; i < source.length; i++) {
+				result[i] = copy(source[i]);
+			}
+			return result;
+		}
+		for (var key in source) {
+			result[key] = copy(source[key]);
+
+		}
+		return result;
+	};
+
+	/**
+	 * @description Slice String
+	 * @param  {[String]} str    [source string]
+	 * @param  {[Number]} length [return length]
+	 * @param  {[String]} symbol [symbol]
+	 * @return {[String]}        [result]
+	 */
+	fn.slice = function(str, length, symbol) {
+		var result = '',
+			sym = symbol || '...';
+
+		if (!str || typeof str !== 'string' || !length) {
+			result = str;
+		} else {
+			if (str.length < length) {
+				sym = '';
+			}
+			result = str.slice(0, length) + sym;
+		}
+
+		return result;
+	};
+
+	/**
+	 * @description In Array
+	 * @param  {Number Or String Or Boolean} item  [item]
+	 * @param  {[Array]} array [source array]
+	 * @return {[Number]}       [position]
+	 */
+	fn.inArray = function(item, array) {
+		for (var i = 0, len = array.length; i < len; i++) {
+			if (array[i] === item) {
+				return i;
+			}
+		}
+		return -1;
+	};
+
+	/**
+	 * @description 变为稠密数据
+	 * @param  {[Array]} array [source array]
+	 * @return {[Array]}       [new array]
+	 */
+	fn.toDenseArray = function(array) {
+		if (Array.isArray(array)) {
+			return array.filter(function(item) {
+				return item;
+			});
+		} else {
+			return array;
+		}
+	};
+
+	/**
+	 * @description Delete Duplicates Array
+	 * @param  {[Array]} array [source array]
+	 * @return {[Array]}       [new  array]
+	 */
+	fn.delDupArray = fn.delArray = function(array) {
+		var targetArray = [];
+		var arr = array || [];
+
+		if (arr.length < 1) {
+			return arr;
+		}
+		for (var i = 0; i < arr.length; i++) {
+			var st = false
+			for (var j = 0; j < targetArray.length; j++) {
+				if (arr[i].toString() == targetArray[j].toString()) {
+					st = true;
+				}
+			}
+			if (!st) {
+				targetArray.push(arr[i]);
+			}
+		}
+		return targetArray;
+	};
+
+	/**
+	 * @description Strim HTML
+	 * @param  {[String]} str [HTML TEXT]
+	 * @return {[String]}     [TEXT]
+	 */
+	fn.strimHtml = function(str) {
+		var reg = /<(?:.|\s)*?>/ig;
+		return str.replace(reg, '');
+	};
+
+	/**
+	 * @description Get Nonce String
+	 * @return {[String]} [Noce String]
+	 */
+	fn.getNonceStr = function(length) {
+		var len = length || 36;
+		return (Math.random().toString(36).substr(2, 100) + Math.random().toString(36).substr(2, 100)).slice(0, len);
+	};
+
+	/**
+	 * @description Get Times Tamp
+	 * @return {[Number]} [description]
+	 */
+	fn.getTimesTamp = function() {
+		return parseInt(new Date().getTime() / 1000) + '';
+	};
+
+	/**
+	 * @description Fix Number
+	 * @param  {[Number]} number [source number]
+	 * @param  {[Number]} length [string length]
+	 * @return {[String]}        [fix number]
+	 * @example
+	 * utils.fixNumber(1,2);//output '01';
+	 * utils.fixNumber(1,3);//output '001';
+	 * 
+	 * utils.fixNumber.size = 4;
+	 * utils.fixNumber(1);//output '0001';
+	 * utils.fixNumber(10);//output '0010';
+	 */
+	fn.fixNumber = function fix(number, length) {
+		var len = length || fix.size || 0;
+		var maxNum = Math.pow(10, len);
+		var str = [];
+		for (var i = number.toString().length; i < maxNum.toString().length - 1; i++) {
+			str.push('0');
+		}
+		return str.join('') + number;
+	};
+
+	/**
+	 * @description get date string
+	 * @param  {[type]} date [description]
+	 * @return {[type]}      [description]
+	 */
+	fn.getDateString = function(date, time) {
+		var utils = Utils(),
+			fix = utils.fixNumber,
+			dateResult = [],
+			timeResult = [],
+			showTime = time === true ? true : false,
+			DATE = date ? new Date(date) : new Date();
+
+		fix.size = 2;
+
+		dateResult.push(DATE.getFullYear());
+		dateResult.push(fix(DATE.getMonth()));
+		dateResult.push(fix(DATE.getDate()));
+		if (showTime) {
+			timeResult.push(fix(DATE.getHours()));
+			timeResult.push(fix(DATE.getMinutes()));
+			timeResult.push(fix(DATE.getSeconds()));
+		}
+
+
+		return dateResult.join('-') + timeResult.join(':');
+	};
+
+	/**
+	 * @description color
+	 * @type {Object}
+	 */
+	var color = {
+		start: {
+			black: '\033[22;30m',
+			red: '\033[22;31m',
+			green: '\033[22;32m',
+			brown: '\033[22;33m',
+			blue: '\033[22;34m',
+			magenta: '\033[22;35m',
+			cyan: '\033[22;36m',
+			gray: '\033[22;37m',
+			darkGray: '\033[01;30m',
+			lightRed: '\033[01;31m',
+			lightGreen: '\033[01;32m',
+			yellow: '\033[01;33m',
+			lightBlue: '\033[01;34m',
+			lightMagenta: '\033[01;35m',
+			lightCyan: '\033[01;36m',
+			white: '\033[01;37m'
+		},
+		end: '\033[0m'
+	};
+	/**
+	 * [log description]
+	 * @param  {[type]} msg   [description]
+	 * @param  {[type]} color [description]
+	 * @return {[type]}       [description]
+	 */
+	function log() {
+		try {
+			console.log.apply(this, arguments);
+		} catch (ev) {}
+	}
+	fn.log = function(msg, style) {
+		log(color.start[style]);
+		log.call(this, msg, color.end);
+	};
+	fn.log.help = function() {
+		var utils = new Utils();
+		for (var key in color.start) {
+			utils.log(key, key);
+		}
+	};
+	fn.success = function() {
+		log(color.start.green);
+		log.apply(this, arguments);
+		log(color.end);
+	};
+	fn.error = function() {
+		log(color.start.red);
+		log.apply(this, arguments);
+		log(color.end);
+	};
+	fn.warn = function() {
+		log(color.start.yellow);
+		log.apply(this, arguments);
+		log(color.end);
+	};
+	fn.info = function() {
+		log(color.start.cyan);
+		log.apply(this, arguments);
+		log(color.end);
+	};
+
+	var utils = new Utils();
+
+	try {
+		module.exports = utils;
+	} catch (ev) {
+		if (root.module && root.module.exports) {
+			root.module.exports = utils;
+		} else {
+			root.utils = utils;
+		}
+	}
+
+})(this);
